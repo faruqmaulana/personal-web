7517fd81b86a5e722f8a9cbe844b0f0381005d77
faruqmaulana <faruqchodot21@gmail.com>
2022-04-10 15:39:23 +0700

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import "../styles/globals.css";
 import "nprogress/nprogress.css"; //styles of nprogress//Binding events.
 import Router from "next/router";
 import NProgress from "nprogress"; //nprogress module
-import Script from "next/script";
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeStart", () =>
@@ -15,14 +14,17 @@ Router.events.on("routeChangeError", () => NProgress.done());
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script id="ga-tag" strategy="lazyOnload" src={process.env.NEXT_GA_TAG} />
-      <Script id="ga-id" strategy="lazyOnload">
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-917GNRTVQH`}
+      />
+      <Script strategy="lazyOnload">
         {`
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
     
-      gtag('config', '${process.env.NEXT_GA_ID}');
+      gtag('config', 'G-917GNRTVQH');
         `}
       </Script>
       <Component {...pageProps} />;
