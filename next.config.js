69fe8cef35780b37ee8d97cb117647e7215d8dff
faruqmaulana <faruqchodot21@gmail.com>
2024-06-28 17:11:17 +0700

    feat: add redirect permanent true to a new portfolio web

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://faruqmaulana.work/',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
